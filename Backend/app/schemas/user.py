758fb7527f8567b2a56cfbc20b2b3d2958d5ca30
bhavesh-125 <bhaveshsawashe0125@gmail.com>
2026-04-24 13:27:12 +0530

    Update User schema to include username and token

--- Backend/app/schemas/user.py
+++ Backend/app/schemas/user.py
@@ -2,15 +2,20 @@ from pydantic import BaseModel, EmailStr
 
 
 class UserCreate(BaseModel):
-    full_name: str
+    username: str
     email: EmailStr
     password: str
 
 
 class UserResponse(BaseModel):
     id: int
-    full_name: str
+    username: str
     email: EmailStr
 
     class Config:
         from_attributes = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
